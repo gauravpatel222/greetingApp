a92cdc2d98ca256afb6403881af21e17477e92b5
Gaurav <gauravgurjar8791@gmail.com>
2025-03-03 11:54:09 +0530

    Path variable

--- src/main/java/org/example/greetingapp/HelloController.java
+++ src/main/java/org/example/greetingapp/HelloController.java
@@ -1,6 +1,7 @@
 package org.example.greetingapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,11 @@ public class HelloController {
     public String sayHelloQuery(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
+    // 3. Say Hello with Name in Path Variable
+    @GetMapping("/hello/param/{name}")
+    public String sayHelloPath(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
 
 }
