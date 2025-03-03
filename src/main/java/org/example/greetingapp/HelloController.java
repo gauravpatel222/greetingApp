f86c8ed1d52f0fd09adfe037b3cd1fcb095ec5e6
Gaurav <gauravgurjar8791@gmail.com>
2025-03-03 14:33:50 +0530

    save it in repo

--- src/main/java/org/example/greetingapp/HelloController.java
+++ src/main/java/org/example/greetingapp/HelloController.java
@@ -1,9 +1,9 @@
 package org.example.greetingapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.greetingapp.Model.Greeting;
+import org.example.greetingapp.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -13,15 +13,40 @@ public class HelloController {
     public String sayHello() {
         return "Hello World!";
     }
+
     // UC2: Say Hello with Name as Query Parameter
     @GetMapping("/hello/query")
     public String sayHelloQuery(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
     // 3. Say Hello with Name in Path Variable
+
     @GetMapping("/hello/param/{name}")
     public String sayHelloPath(@PathVariable String name) {
         return "Hello, " + name + "!";
+    }
+
+    // 4
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    @PostMapping("/greeting")
+    public String generateGreeting(@RequestBody Greeting greeting) {
+        String message;
+
+        if (greeting.getFirstName() != null && greeting.getLastName() != null) {
+            message = "Hello, " + greeting.getFirstName() + " " + greeting.getLastName() + "!";
+        } else if (greeting.getFirstName() != null) {
+            message = "Hello, " + greeting.getFirstName() + "!";
+        } else if (greeting.getLastName() != null) {
+            message = "Hello, " + greeting.getLastName() + "!";
+        } else {
+            message = "Hello, World!";
+        }
+
+        greeting.setMessage(message);
+        greetingRepository.save(greeting);
+        return message;
     }
 
 
