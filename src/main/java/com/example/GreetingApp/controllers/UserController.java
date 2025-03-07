711140bfcb4dd8bafbd25ec76222c0ed119ff905
Gaurav <gauravgurjar8791@gmail.com>
2025-03-07 10:57:57 +0530

    swagger

--- src/main/java/com/example/GreetingApp/controllers/UserController.java
+++ src/main/java/com/example/GreetingApp/controllers/UserController.java
@@ -20,20 +20,24 @@ public class UserController {
 
     //UC9 --> For Registration of a user
     @PostMapping(path = "/register")
-    public String register(@RequestBody AuthUserDTO user){
+    public String register(@RequestBody AuthUserDTO user) {
         return iAuthInterface.register(user);
     }
 
     //UC10 --> For User Login
-    @PostMapping(path ="/login")
-    public String login(@RequestBody LoginDTO user){
+    @PostMapping(path = "/login")
+    public String login(@RequestBody LoginDTO user) {
         return iAuthInterface.login(user);
     }
+
     //UC11 --> For sending mail to another person
     @PostMapping(path = "/sendMail")
-    public String sendMail(@RequestBody MailDTO message){
+    public String sendMail(@RequestBody MailDTO message) {
         emailService.sendEmail(message.getTo(), message.getSubject(), message.getBody());
         return "Mail sent";
 
 
     }
+    //UC12 --> Added Swagger Config to use Swagger at url(/swagger)
+
+}
